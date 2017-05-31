f735a98edec45bee13e7a9d1dde9766cccc65ca4
Thibault7 <thibault.bouckaert@student.howest.be>
2017-05-31 20:16:43 +0200

    test

--- lcd.py
+++ lcd.py
@@ -26,9 +26,7 @@ lcd_d7        = 22
 lcd_columns = 16
 lcd_rows    = 2
 
-# Alternatively specify a 20x4 LCD.
-# lcd_columns = 20
-# lcd_rows    = 4
+
 
 # Initialize the LCD using the pins above.
 lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
